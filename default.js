292d232e7de60033bbe0f57ff7e2582fe6c371ae
Michael Mueller <michael.mueller@binary.com>
2019-04-01 07:53:35 +1100

    dont wipe out other text, ignore functions on fq functions, add cutom mapping got BOM::events

--- default.js
+++ default.js
@@ -2,7 +2,8 @@
 function add_links (){
 	var matches = document.querySelectorAll("td.blob-code span, table.js-file-line-container td");
 	matches.forEach(function(e){ 
-		var text = e.textContent.match(/(\w+::\w+)+\w/);  
+		var getModule = /(\w+::[A-Z]\w+)+\w/;
+		var text = e.textContent.match(getModule);  
 		if ( text != null) {
 			console.log(text[0]);
                         var module = text[0].split('::');
@@ -11,14 +12,19 @@ function add_links (){
 			if (prefix == 'Binary'){
 				url = 'https://github.com/regentmarkets/binary-websocket-api/blob/master/lib/'+text[0].replace(/::/g, '/') +'.pm';
 			} else if (prefix == 'BOM') {
-				var inner = module[0] +'-'+module[1];
+				var second_name = module[1];
+				if (module[1] == 'Event') { 
+					second_name = 'events';
+                                }
+				var inner = module[0] +'-'+second_name;
 				inner = inner.toLowerCase();
+				
 				url = 'https://github.com/regentmarkets/'+inner+'/blob/master/lib/'+text[0].replace(/::/g, '/') +'.pm'
 
 			}else { 
 				url = 'https://metacpan.org/pod/'+text[0];
 			} 
-			e.innerHTML = '<a href="'+url+'">'+ text[0] +'</a>' } })
+			e.innerHTML = e.textContent.replace(getModule,'<a href="'+url+'">'+ text[0] +'</a>' )} })
     }
 
 
